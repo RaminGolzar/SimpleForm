b3a392bbce28a20fc54d2498adccacd5a0cd726a
Eraj <eraj@email.ir>
2023-06-02 16:12:27 +0430

    Saving ToDo task in a separated file

--- SimpleForm.php
+++ SimpleForm.php
@@ -1,19 +1,6 @@
 <?php
 namespace App\Libraries\Form;
 
-/* ToDo:
- *
- * bayad kari kard ke input type hidden manande sayere
- * input ha dar yek section tag jasaz shavad
- *
- * background color dar submit link ha kar nemikonad
- *
- * set kardane action haye motefavet baraye submit haye (yek) form
- *
- * This entity is related to the mingifire icon, which is very
- * useful for submit input. -> &#128269;
- */
-
 /* this class required form helper
  *
  */
@@ -77,23 +64,6 @@ class SimpleForm
     private function row (string $content): string {
         return "<section class='w3-margin-top w3-row'>$content</section>";
     }
-
-    /* ToDo:
-     * bayad emkani faraham shavad ta betavan yek content ra
-     * as har type va noei ke bashad ra niz be result ezafe kard
-     * masalan yek link ya yek ask ya ...
-     */
-
-    /* ToDo:
-     * faraham kardane afzudane har attr kharej as parametr haye tarif
-     * shodeh hala in attr value dashte bashad ya manande check
-     * nadashte bashad
-     */
-
-    /* ToDo:
-     * bayad emkani faraham kard ke betavan null value be attr value
-     * input ha ekhtesas dad.
-     */
 
     /** generate input type text
      *
@@ -163,12 +133,6 @@ class SimpleForm
 
         return $this;
     }
-
-    /* ToDo:
-     * bayad kari kard ke betavan baraye har submit yek action
-     * mojaza tarif kard baraye zamani ke chand submit dar yek form
-     * vojud darad.
-     */
 
     /** generate input type submit
      *
@@ -354,10 +318,6 @@ class SimpleForm
         return $this;
     }
 
-    /* ToDo:
-     * Should fix submit linkable background
-     */
-
     /** load and get complete html form result
      * finally methid in work
      *
@@ -413,29 +373,3 @@ class SimpleForm
     }
 
 }
-
-/* TODO:
- *      + dar zamani ke ba submit() method yek link button
- *         sakhte mishavad bgColor an moshkel darad.
- *
- *      + faraham kardane emkane 2 sotune shodane field ha
- *
- *      1- create form method
- *            (action, attr, heading, fieldset, border)
- *
- *      2- bayad method haye text & password ba
- *            parametr haye array kar konad ta betavan ba yek
- *            fara khavani chand input eijad kard.
- *
- *      3- create module for generate search form (pro)
- *
- *      4- vaghti ke dar dakhele yek table yek submit gharar
- *            dahim be dalile margin dashtan zahere badi peyda
- *            mikonad.
- *
- *      5- faraham kardane emkani baraye eijad tonde form hayi
- *            ke faghat shamele hidden va buttons bashad
- *
- *      6- faraham kardane emkane nemayeshe error message
- *            dar zire input ha
- */
