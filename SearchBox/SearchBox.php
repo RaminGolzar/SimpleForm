ce0b2c53746a44f18bcc7a3af2983ce2d3d56d99
Eraj <eraj@email.ir>
2023-06-03 20:52:36 +0430

    Makeing some changes in SearchBox.php

--- SearchBox/SearchBox.php
+++ SearchBox/SearchBox.php
@@ -1,36 +1,29 @@
 <?php
-namespace App\Libraries\Form\LSearch;
+namespace SimpleForm\SearchBox;
 
-/** 
- * Class name is Little Search (LSearch)
- * 
- * This class usful for generate a search form
- */
-class LSearch
+class SearchBox
 {
+
     use Handlers\Text;
     use Handlers\Submit;
-    
-    /* ------------------------------------------------------------ */
-    
+
     /**
      * Submit align <p>valid value is: left | right</p>
-     * 
+     *
      * @var string
      */
     public string $align = 'left';
-    
-    /* ------------------------------------------------------------ */
-    
+
     /**
      * Generate search box
-     * 
+     *
      * @return string
      */
-    public function __invoke (string $action='', string $method='post'): string {
+    public function create_searchbox (string $action = '' , string $method = 'post'): string {
         $txt = $this->generate_text ();
         $btn = $this->generate_submit ();
-        
+
         return "<form action='$action' method='$method' class='w3-container'>$btn $txt</form>";
     }
+
 }
